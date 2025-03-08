ecea48b42fcf208caaca76ac060fe8f2f1bf04d6
nidaxibi_79 <201304768+nidaxibi-79@users.noreply.github.com.>
2025-03-08 16:17:34 +0900

    #

--- src/types/option/index.ts
+++ src/types/option/index.ts
@@ -4,7 +4,7 @@ import { Some } from "./some";
 export type Option<T> = Some<T> | None<T>;
 
 type OptionT = {
-    from<T>(v: T | null | undefined): Option<T>;
+    from<T>(v: T | null | undefined): Option<NonNullable<T>>;
     from<T>(v: null | undefined): None<T>;
     from<T>(v: T): Some<T>;
     all<T>(arr: Option<T>[]): Option<T[]>;
