1f5f13e20939e15ac986016c99b5934537965aec
nidaxibi_79 <201304768+nidaxibi-79@users.noreply.github.com.>
2025-03-08 16:18:32 +0900

    #

--- src/ns.ts
+++ src/ns.ts
@@ -1,4 +1,4 @@
 export { Option, Some, None } from "./types/option/index";
-export { Result, Ok, Err } from "./types/result/index"
+export { Result, Ok, Err } from "./types/result/index";
 export { PointerHandler } from "./event/pointer";
 export { Frame } from "./event/frame";
