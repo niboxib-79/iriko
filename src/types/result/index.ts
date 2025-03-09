5af6856223cac62276305ccd7658168924540b05
niboxib_79 <201304768+nidaxibi-79@users.noreply.github.com.>
2025-03-09 11:13:55 +0900

    #

--- src/types/result/index.ts
+++ src/types/result/index.ts
@@ -6,7 +6,7 @@ export type Result<T, E> = Ok<T, E> | Err<T, E>;
 type ResultT = {
     try<T, E = unknown>(f: () => T): Result<T, E>;
     try_async<T, E = unknown>(f: () => Promise<T>): Promise<Result<T, E>>;
-    try_promise<T, E = unknown>(p: Promise<T>): Promise<Result<T, E>>;
+    try_promise<T, E = unknown>(p: Promise<T>): Promise<Result<Awaited<T>, E>>;
     all<T, E>(arr: Result<T, E>[]): Result<T[], E>;
     any<T, E>(arr: Result<T, E>[]): Result<T, E[]>;
 };
