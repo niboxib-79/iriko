6ca997a0c4dbd0b4f4b80dfbdd5b07619317b557
niboxib_79 <201304768+niboxib-79@users.noreply.github.com>
2025-03-16 13:03:41 +0900

    #

--- src/ns.ts
+++ src/ns.ts
@@ -1,4 +1,6 @@
 export { Option, Some, None } from "./types/option/index";
 export { Result, Ok, Err } from "./types/result/index";
+export { OptionAsync, SomeAsync, NoneAsync } from "./types/option_async/index";
+export { ResultAsync, OkAsync, ErrAsync } from "./types/result_async/index";
 export { PointerHandler } from "./event/pointer";
 export { Frame } from "./event/frame";
--- src/types/result_async/index.ts
+++ src/types/result_async/index.ts
@@ -1,4 +1,6 @@
-import type { ErrAsync } from "./err";
-import type { OkAsync } from "./ok";
+import { ErrAsync } from "./err";
+import { OkAsync } from "./ok";
 
 export type ResultAsync<T, E> = OkAsync<T, E> | ErrAsync<T, E>;
+
+export { ErrAsync, OkAsync };
