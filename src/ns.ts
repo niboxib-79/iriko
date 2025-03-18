9076c3099c25bbc13f36b1e2db42d607b0d18377
niboxib_79 <201304768+niboxib-79@users.noreply.github.com>
2025-03-18 18:43:05 +0900

    #

--- src/ns.ts
+++ src/ns.ts
@@ -1,4 +1,20 @@
-export { UnwrapO, Option, Some, None } from "./types/option/index";
-export { UnwrapR, Result, Ok, Err } from "./types/result/index";
+export {
+    OptionAsync,
+    SomeAsync,
+    NoneAsync,
+    UnwrapO,
+    Option,
+    Some,
+    None,
+} from "./types/option/index";
+export {
+    ResultAsync,
+    OkAsync,
+    ErrAsync,
+    UnwrapR,
+    Result,
+    Ok,
+    Err,
+} from "./types/result/index";
 export { PointerHandler } from "./event/pointer";
 export { Frame } from "./event/frame";
